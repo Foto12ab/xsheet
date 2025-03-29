fc3c8400abfbf5cfcbb39e964f47e7ddf3458797
Mingzhe Zou <zoumingzhe@outlook.com>
2025-03-30 02:35:39 +0800

    bump version to 0.1

--- xkits_sheet/attribute.py
+++ xkits_sheet/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xkits-sheet"
-__version__ = "0.1.alpha.1"
+__version__ = "0.1"
 __urlhome__ = "https://github.com/bondbox/xsheet/"
 __description__ = "Form module"
 
